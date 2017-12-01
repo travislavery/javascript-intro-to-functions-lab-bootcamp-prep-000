df331f9ba69789e698af3cb89fbb22278a355159
Travis Lavery <travisalavery@gmail.com>
2017-12-01 21:43:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,12 +17,21 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   var uppercase = string.toUpperCase()
   var lowercase = string.toLowerCase()
+<<<<<<< HEAD
   var loveGrandma = 'I love you, Grandma.'
   if (string === lowercase) {
     return "I can't hear you!"
   } else if (string === uppercase) {
     return "YES INDEED!"
   } else if (string === loveGrandma) {
+=======
+  if (string === lowercase) {
+    var response = "I can't hear you!"
+    return response
+  } else if (string === uppercase) {
+    return "YES INDEED!"
+  } else if (string === 'I love you Grandma.') {
+>>>>>>> 03cb281f1ed3e80f066cb6c9e051d0ef7b4b5c48
     return "I love you, too."
   }
 }
